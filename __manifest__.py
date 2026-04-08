a4ce9d391a45cff66dd7b2bb2ac91b17a4a97440
angelodgz <deguzmangelo83@gmail.com>
2026-04-08 10:24:31 +0800

    Manifest file for helpdesk_ticket module

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Helpdesk Ticket',
+    'version': '1.0',
+    'summary': 'Basic module',
+    'author': 'You',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}

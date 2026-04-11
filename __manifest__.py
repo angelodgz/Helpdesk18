574d59ae64399a540c8e370b0aacaa1eea406d6d
Gitmar2 <jhomarvaron16@gmail.com>
2026-04-11 18:25:15 +0800

    fix: HelpDesk Main App

--- __manifest__.py
+++ __manifest__.py
@@ -5,14 +5,15 @@
     'author': 'You',
     'category': 'Tools',
     'depends': ['base', 'mail', 'hr'],
-    'data': [
-        'security/ir.model.access.csv',
+'data': [
+        'security/groups.xml',            # DAPAT MAUNA PARA SA ROLES
+        'security/ir.model.access.csv',   # SUNOD ANG PERMISSIONS
         'data/sequence.xml',
         'data/stages.xml',
-        'views/menus.xml',
-        'views/ticket_views.xml',
+        'views/ticket_views.xml',         # LOAD VIEWS BAGO MENU
         'views/stage_views.xml',
         'views/employee_inherit.xml',
+        'views/menus.xml',                # DAPAT HULI ANG MENU
     ],
     'installable': True,
     'application': True,
